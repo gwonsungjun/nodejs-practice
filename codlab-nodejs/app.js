89f0f0abdc3063405b6c9c7c7caf5e1b23188af7
Sungjun <sungjunpizz@gmail.com>
2019-03-28 15:48:31 +0900

    update app.js
    특정 사용자 조회 API

--- codlab-nodejs/app.js
+++ codlab-nodejs/app.js
@@ -44,3 +44,18 @@ let users = [
 ]
 
 app.get('/users', (req, res) => res.json(users));
+
+app.get('/users/:id', (req, res) => {
+
+    const id = parseInt(req.params.id, 10);
+    if (!id) {
+        return res.status(400).json({error: 'Incorrect id'});
+    }
+
+    let user = users.filter(user => user.id === id)[0]
+    if(!user) {
+        return res.status(404).json({error: 'Unknown user'});
+    }
+
+    return res.json(user);
+});
